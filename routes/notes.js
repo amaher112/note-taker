6f365451cffbc9d5def5a0084ece47fa50127e2e
amaher <amaher112@su.edu>
2024-02-07 17:44:05 -0500

    added delete file

--- routes/notes.js
+++ routes/notes.js
@@ -2,25 +2,25 @@ const notes = require("express").Router();
 const uuid = require("../helpers/uuid.js");
 const { readAndAppend, readFromFile } = require("../helpers/fsUtils.js");
 
-// GET /api/notes reads db.json
-
+// GET request reads db.json
 notes.get("/notes", (req, res) => {
   console.info(`${req.method} request received for notes`);
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
-// POST /api/notes
+
+// POST request for notes
 notes.post("/notes", (req, res) => {
   console.info(`${req.method} request received to notes`);
   const { title, text } = req.body;
 
-  // If all the required properties are present, then create a newNote
+  // If all the required properties are present, then create a newNote with a unique ID
   if (title && text) {
     const newNote = {
       title,
       text,
       note_id: uuid(),
     };
-
+    // Appends the newNote to the db.json file
     readAndAppend(newNote, "./db/db.json");
 
     const response = {
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-// const tasks = require('./db/db.json');
 const api = require("./routes/notes.js");
 
 const PORT = process.env.port || 3001;
@@ -14,11 +13,11 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET /notes returning notes.html
+// GET /notes returns notes.html
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
-// GET* returns index.html
+// Anything else returns index.html
 app.get("/*", (req, res) =>
 res.sendFile(path.join(__dirname, "/public/index.html"))
 );
