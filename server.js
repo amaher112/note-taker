a9a9e75ed6b61f2f52922617c1dbd8c211327d21
amaher <amaher112@su.edu>
2024-02-08 18:24:55 -0500

    fixed routing for index

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 // Anything else returns index.html
-app.get("/*", (req, res) =>
+app.get("*", (req, res) =>
 res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
